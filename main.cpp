6c94e9f329ff9cbd724ff79fc04ffab2f02bc6de
iago8 <iago8mendes@hotmail.com>
2013-10-24 20:04:47 -0200

    criacao da main

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/* O que o programa faz?
+ * R: ler uma expressao aritmetica em notacao postfix, e 
+ fornecer o resultado da expressao.
+ * Arquivos Anexos: 
+ * Disciplina: Estrutura de Dados e Algoritmos - 193704
+ * Professor: Nilton Correia da Silva
+ * Turma: B
+ * Tarefa: Trabalho Pratico (Pilhas)
+ * Grupo: 8
+ * Data de Termino: 22/10/2013
+ * Integrantes:
+ * -> Dennis Lúcio Merli Rodrigues
+ * --> GitHub: https://github.com/neneds
+ * -> Iago Mendes Leite
+ * --> GitHub: https://github.com/iago8
+ * -> Ricardo Gonçalves Teixeira
+ * --> GitHub: https://github.com/ricardogtx
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+system ("PAUSE"); 
+return 0;   
+}
